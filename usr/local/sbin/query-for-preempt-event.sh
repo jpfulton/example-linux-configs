d8d78bde7ed147e1ecbe5637673e415f5efe3928
J. Patrick Fulton <jpatrick.fulton@gmail.com>
2023-07-23 17:31:05 -0500

    Add graceful shutdown script for spot vm instances.

---
+++ usr/local/sbin/query-for-preempt-event.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+ENDPOINT_IP="169.254.169.254";
+API_VERSION="2020-07-01";
+
+HEADER="Metadata:true";
+ENDPOINT_URL="http://${ENDPOINT_IP}/metadata/scheduledevents?api-version=${API_VERSION}";
+
+if [ "$(curl -s -H ${HEADER} ${ENDPOINT_URL} | grep -c Preempt)" -ge 1 ]
+    then
+        echo "Preempt event found...";
+	    shutdown;
+fi
