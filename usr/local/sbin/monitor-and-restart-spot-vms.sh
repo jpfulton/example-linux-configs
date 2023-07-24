b415c459b2c19bc64b058423d9645934fb2f6f2b
J. Patrick Fulton <jpatrick.fulton@gmail.com>
2023-07-24 11:10:52 -0500

    Refine monitoring script and clean up.

--- usr/local/sbin/monitor-and-restart-spot-vms.sh
+++ usr/local/sbin/monitor-and-restart-spot-vms.sh
@@ -5,18 +5,28 @@ set -e;
 
 RESOURCE_GROUP="personal-network"
 
-DEALLOCATED_NAMES=$(az vm list -g ${RESOURCE_GROUP} -d -o tsv --query "[?billingProfile.maxPrice != null && powerState == 'VM deallocated'].{Name:name}");
+echo "Querying for deallocated VMs...";
+SPOT_ALLOCATION_QUERY="
+	[?
+		billingProfile.maxPrice != null && 
+		powerState == 'VM deallocated'
+	].{Name:name}";
+	
+DEALLOCATED_VM_NAMES=$(az vm list -g ${RESOURCE_GROUP} -d -o tsv --query "${SPOT_ALLOCATION_QUERY}");
 
-NAMES_ARRAY=($DEALLOCATED_NAMES);
-ARRAY_LENGTH=${#NAMES_ARRAY[@]};
+VM_NAMES_ARRAY=($DEALLOCATED_VM_NAMES);
+ARRAY_LENGTH=${#VM_NAMES_ARRAY[@]};
 
 if [ $ARRAY_LENGTH -ge 1 ]
-    then
-	echo "Found ${ARRAY_LENGTH} deallocated VM(s).";
+	then
+		echo "Found ${ARRAY_LENGTH} deallocated VM(s).";
 
-	for (( i=0; i<$ARRAY_LENGTH; i++ )); 
-	do
-	    echo "Attempting to start VM: ${NAMES_ARRAY[i]}";
-	    az vm start -g ${RESOURCE_GROUP} --name ${NAMES_ARRAY[i]};
-        done
+		for (( i=0; i<$ARRAY_LENGTH; i++ )); 
+		do
+			echo "Attempting to start VM: ${VM_NAMES_ARRAY[i]}";
+			az vm start -g ${RESOURCE_GROUP} --name ${VM_NAMES_ARRAY[i]};
+		done
+
+	else
+		echo "No deallocated VMs discovered.";
 fi
