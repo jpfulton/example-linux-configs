3819d0fe64e56b8f23c9ee45c1bb9d7087fedd33
J. Patrick Fulton <jpatrick.fulton@gmail.com>
2023-07-24 22:52:27 -0500

    Handle lock file and service principal login.

--- usr/local/sbin/monitor-and-restart-spot-vms.sh
+++ usr/local/sbin/monitor-and-restart-spot-vms.sh
@@ -1,9 +1,70 @@
 #!/usr/bin/env bash
 
-# exit on error
-set -e;
+RESOURCE_GROUP="personal-network";
+SP_APP_ID="a77793d9-d99f-4309-a629-150db0977169"; # REPLACE WITH YOUR SP APPID
+SP_TENANT_ID="d3a3db1f-a65f-427b-a6eb-b74b29cd2d56"; # REPLACE WITH YOUR SP TENANTID
 
-RESOURCE_GROUP="personal-network"
+# ensure this script is running as root or sudo
+if [ $(id -u) -ne 0 ]
+  then
+    echo "This script must be run as root or in a sudo context. Exiting.";
+    exit 1;
+fi
+
+CONFIG_DIR="/etc/azure";
+LOCK_FILE="${CONFIG_DIR}/deallocation-monitor.lock";
+if [ ! -d $CONFIG_DIR ]
+	then
+		echo "Configuration directory does not exist. Creating...";
+		mkdir $CONFIG_DIR;
+fi
+
+# This script can be long running. Use a lock file.
+if [ -f $LOCK_FILE ]
+	then
+		echo "Found lock file.";
+		RUNNING_PID=$(cat $LOCK_FILE);
+		if [ ! -n "$(ps -p $RUNNING_PID -o pid=)" ]
+			then
+				echo "Lock file is stale. Removing and continuing...";
+				rm $LOCK_FILE;
+			else
+				echo "Another instance of this script is running at PID: ${RUNNING_PID}. Exiting...";
+				exit 0;
+		fi
+	else
+		touch $LOCK_FILE;
+		echo $BASHPID > $LOCK_FILE;
+fi
+
+# Attempt az login using service principal
+SP_PEM_FILE="${CONFIG_DIR}/sp.pem";
+if [ ! -f $SP_PEM_FILE ]
+	then
+		echo "Azure Service Principal PEM not found. Create one prior to using this script...";
+		rm $LOCK_FILE;
+		exit 1;
+	else
+		az account show > /dev/null 2>&1;
+		if [ $? -ne 0 ]
+			then
+				echo "Logging in with Azure Service Principal PEM...";
+				az login \
+					--service-principal \
+					--username ${SP_APP_ID} \
+					--tenant ${SP_TENANT_ID} \
+					--password ${SP_PEM_FILE} > /dev/null 2>&1;
+
+				if [ $? -ne 0 ]
+					then
+						echo "Error: Unable to login. Exiting.";
+						rm $LOCK_FILE;
+						exit 1;
+				fi
+			else
+				echo "Already logged in to Azure. Continuing...";
+		fi
+fi
 
 echo "Querying for deallocated VMs tagged for restart after eviction...";
 SPOT_ALLOCATION_QUERY="
@@ -32,3 +93,6 @@ if [ $ARRAY_LENGTH -ge 1 ]
 	else
 		echo "No deallocated VMs tagged for restart after eviction discovered.";
 fi
+
+## Remove lock file.
+rm $LOCK_FILE;
