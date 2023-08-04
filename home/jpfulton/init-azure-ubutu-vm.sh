a0e056b0c8803cf9895a7d6ef9d673a2fee86fa0
J. Patrick Fulton <jpatrick.fulton@gmail.com>
2023-08-04 18:49:59 -0500

    Update with node, yarn and sms-notify-cli installation steps.

--- home/jpfulton/init-azure-ubutu-vm.sh
+++ home/jpfulton/init-azure-ubutu-vm.sh
@@ -56,6 +56,72 @@ if [ ! -f ${MOTD_PATH}${MOTD_FILE} ]
     echo;
 fi
 
+# Install Node as needed
+which node;
+if [ $? -eq 0 ]
+  then
+    NODE_VERSION=$(node --version);
+    if [ $NODE_VERSION == "v12.22.9" ]
+      then
+        echo "Default node package detected. Removing.";
+        apt remove nodejs;
+        apt autoremove;
+      else
+        echo "Detected alternate version of node: ${NODE_VERSION}";
+        echo "Ensure that version is above v18.0.0 or manually use nvm.";
+    fi
+  else
+    echo "Node not detected. Preparing installation of node v18.x.";
+
+    curl -sL  https://deb.nodesource.com/setup_18.x | bash -;
+    apt install -y nodejs;
+fi
+
+# Install Yarn as needed
+which yarn;
+if [ $? -eq 1 ]
+  then
+    echo "Yarn not detected. Preparing to install.";
+
+    curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | gpg --dearmor | tee /usr/share/keyrings/yarnkey.gpg >/dev/null;
+    echo "deb [signed-by=/usr/share/keyrings/yarnkey.gpg] https://dl.yarnpkg.com/debian stable main" | tee /etc/apt/sources.list.d/yarn.list;
+    apt update;
+    apt install -y yarn;
+  else
+    YARN_VERSION=$(yarn --version);
+    echo "Found yarn version: ${YARN_VERSION}";
+fi
+
+# Install or upgrade sms-notify-cli utility
+which sms-notify-cli;
+if [ $? -eq 1 ]
+  then
+    echo "sms-notify-cli utility not detected. Preparing to install.";
+    yarn global add @jpfulton/net-sms-notifier-cli;
+  else
+    echo "Found sms-notify-cli utility. Attempting update.";
+    yarn global upgrade @jpfulton/net-sms-notifier-cli@latest;
+fi
+
+# Initialize sms-notify-cli configuration
+NOTIFIER_CONFIG="/etc/sms-notifier/notifier.json";
+if [ -f $NOTIFIER_CONFIG ]
+  then
+    echo "Found notifier configuration. Validating with current version.";
+
+    sms-notify-cli validate;
+    if [ $? -eq 0 ]
+      then
+        echo "Configuration file validation passes on current version.";
+      else
+        echo "Invalid configuration file. Manually correct.";
+    fi
+  else
+    echo "No notifier configuration found. Initializing...";
+    echo "Manual configuration to the ${NOTIFIER_CONFIG} file will be required.";
+    sms-notify-cli init;
+fi
+
 # Set up eviction query and shutdown script
 EVICTION_QUERY_CRON_SNIPPET_FILE="preempt-query";
 EVICTION_QUERY_SCRIPT="query-for-preempt-event.sh";
