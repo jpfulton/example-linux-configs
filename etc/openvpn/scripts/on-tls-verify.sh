9799053f11674691e2ea5e5b1195fa02490af24b
J. Patrick Fulton <jpatrick.fulton@gmail.com>
2023-08-05 15:06:51 -0500

    Expand verification script.

--- etc/openvpn/scripts/on-tls-verify.sh
+++ etc/openvpn/scripts/on-tls-verify.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
-sudo sms-notify-cli vpn-attempt -i ${untrusted_ip} -n ${X509_0_CN} -e ${X509_0_emailAddress};
+[ $# -lt 2 ] && exit 1;
+ 
+# if the depth is non-zero , continue processing 
+[ "$1" -ne 0 ] && exit 0;
+
+sudo sms-notify-cli vpn-attempt -i ${untrusted_ip} -n "$2";
+exit 0;
