e7669e0a8b75e9ebae25dcaa9232726e16fcdd6e
J. Patrick Fulton <jpatrick.fulton@gmail.com>
2023-08-04 19:03:44 -0500

    Add log file record of evictions.

--- usr/local/sbin/query-for-preempt-event.sh
+++ usr/local/sbin/query-for-preempt-event.sh
@@ -2,6 +2,7 @@
 
 ENDPOINT_IP="169.254.169.254";
 API_VERSION="2020-07-01";
+LOG_FILE="/var/log/azure-eviction.log";
 
 HEADER="Metadata:true";
 ENDPOINT_URL="http://${ENDPOINT_IP}/metadata/scheduledevents?api-version=${API_VERSION}";
@@ -10,6 +11,8 @@ if [ "$(curl -s -H ${HEADER} ${ENDPOINT_URL} | grep -c Preempt)" -ge 1 ]
     then
         echo "Azure preempt event found... Shutting down.";
         wall "Azure preempt event found... Shutting down cleanly prior to eviction.";
+
+        echo "$(date) - EVICTION - Azure preempt event discovered." >> $LOG_FILE;
         sms-notify-cli eviction $(hostname);
 
         sleep 5;
