8ac182cadef9250e177ba4d456e0e84018e39507
J. Patrick Fulton <jpatrick.fulton@gmail.com>
2023-08-07 21:48:40 -0500

    Refactor to use functions.

--- home/jpfulton/init-azure-ubutu-vm.sh
+++ home/jpfulton/init-azure-ubutu-vm.sh
@@ -10,175 +10,208 @@ fi
 BASE_REPO_URL="https://raw.githubusercontent.com/jpfulton/example-linux-configs/main";
 HOME_DIR="/home/jpfulton/";
 
-# Upgrade base image packages
-echo "Upgrading base packages...";
-apt update;
-apt upgrade -y;
-echo "---";
-echo;
+upgrade-base-packages () {
+  # Upgrade base image packages
+  echo "Upgrading base packages...";
+  apt update;
+  apt upgrade -y;
+  echo "---";
+  echo;
+}
 
-# Set up local firewall basics
-DEFAULTS_PATH="/etc/default/";
-UFW_DEFAULTS_FILE="ufw";
-if [ $(ufw status | grep -c inactive) -ge 1 ]
-  then
-    echo "Local firewall is inactive. Configuring and enabling with SSH rule...";
+setup-firewall-defaults-and-ssh () {
+  # Set up local firewall basics
+  local DEFAULTS_PATH="/etc/default/";
+  local UFW_DEFAULTS_FILE="ufw";
+  if [ $(ufw status | grep -c inactive) -ge 1 ]
+    then
+      echo "Local firewall is inactive. Configuring and enabling with SSH rule...";
 
-    wget ${BASE_REPO_URL}${DEFAULTS_PATH}${UFW_DEFAULTS_FILE} -O ${UFW_DEFAULTS_FILE};
-    mv ${UFW_DEFAULTS_FILE} ${DEFAULTS_PATH};
+      wget ${BASE_REPO_URL}${DEFAULTS_PATH}${UFW_DEFAULTS_FILE} -O ${UFW_DEFAULTS_FILE};
+      mv ${UFW_DEFAULTS_FILE} ${DEFAULTS_PATH};
 
-    ufw allow ssh;
-    ufw show added;
-    ufw enable;
-    ufw status numbered;
-  
-  else
-    echo "Local fireall is active. No configuration or rules applied.";
-fi
-echo "---";
-echo;
+      ufw allow ssh;
+      ufw show added;
+      ufw enable;
+      ufw status numbered;
+    
+    else
+      echo "Local fireall is active. No configuration or rules applied.";
+  fi
+  echo "---";
+  echo;
+}
 
-# Set up custom MOTD script
-MOTD_PATH="/etc/update-motd.d/";
-MOTD_FILE="01-custom";
-if [ ! -f ${MOTD_PATH}${MOTD_FILE} ]
-  then
-    echo "Setting up custom MOTD script...";
+setup-motd () {
+  # Set up custom MOTD script
+  local MOTD_PATH="/etc/update-motd.d/";
+  local MOTD_FILE="01-custom";
+  if [ ! -f ${MOTD_PATH}${MOTD_FILE} ]
+    then
+      echo "Setting up custom MOTD script...";
 
-    apt install -y neofetch inxi;
-    wget ${BASE_REPO_URL}${MOTD_PATH}${MOTD_FILE} -O ${MOTD_FILE};
-    chmod a+x ./${MOTD_FILE};
-    mv ./${MOTD_FILE} ${MOTD_PATH}${MOTD_FILE};
+      apt install -y neofetch inxi;
+      wget ${BASE_REPO_URL}${MOTD_PATH}${MOTD_FILE} -O ${MOTD_FILE};
+      chmod a+x ./${MOTD_FILE};
+      mv ./${MOTD_FILE} ${MOTD_PATH}${MOTD_FILE};
 
-    echo "---";
-    echo;
-fi
+      echo "---";
+      echo;
+  fi
+}
 
-# Install Node as needed
-which node >> /dev/null;
-if [ $? -eq 0 ]
-  then
-    NODE_VERSION=$(node --version);
-    if [ $NODE_VERSION == "v12.22.9" ]
-      then
-        echo "Default node package detected. Removing.";
-        apt remove nodejs;
-        apt autoremove;
-      else
-        echo "Detected alternate version of node: ${NODE_VERSION}";
-        echo "Ensure that version is above v18.0.0 or manually use nvm.";
-    fi
-  else
-    echo "Node not detected. Preparing installation of node v18.x.";
+setup-nodejs () {
+  # Install Node as needed
+  which node >> /dev/null;
+  if [ $? -eq 0 ]
+    then
+      local NODE_VERSION=$(node --version);
+      if [ $NODE_VERSION == "v12.22.9" ]
+        then
+          echo "Default node package detected. Removing.";
+          apt remove nodejs;
+          apt autoremove;
+        else
+          echo "Detected alternate version of node: ${NODE_VERSION}";
+          echo "Ensure that version is above v18.0.0 or manually use nvm.";
+      fi
+    else
+      echo "Node not detected. Preparing installation of node v18.x.";
 
-    curl -sL  https://deb.nodesource.com/setup_18.x | bash -;
-    apt install -y nodejs;
-fi
+      curl -sL  https://deb.nodesource.com/setup_18.x | bash -;
+      apt install -y nodejs;
+  fi
+}
 
-# Install Yarn as needed
-which yarn >> /dev/null;
-if [ $? -eq 1 ]
-  then
-    echo "Yarn not detected. Preparing to install.";
+setup-yarn () {
+  # Install Yarn as needed
+  which yarn >> /dev/null;
+  if [ $? -eq 1 ]
+    then
+      echo "Yarn not detected. Preparing to install.";
 
-    curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | gpg --dearmor | tee /usr/share/keyrings/yarnkey.gpg >/dev/null;
-    echo "deb [signed-by=/usr/share/keyrings/yarnkey.gpg] https://dl.yarnpkg.com/debian stable main" | tee /etc/apt/sources.list.d/yarn.list;
-    apt update;
-    apt install -y yarn;
-  else
-    YARN_VERSION=$(yarn --version);
-    echo "Found yarn version: ${YARN_VERSION}";
-fi
+      curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | gpg --dearmor | tee /usr/share/keyrings/yarnkey.gpg >/dev/null;
+      echo "deb [signed-by=/usr/share/keyrings/yarnkey.gpg] https://dl.yarnpkg.com/debian stable main" | tee /etc/apt/sources.list.d/yarn.list;
+      apt update;
+      apt install -y yarn;
+    else
+      local YARN_VERSION=$(yarn --version);
+      echo "Found yarn version: ${YARN_VERSION}";
+  fi
+}
 
-# Install or upgrade sms-notify-cli utility
-which sms-notify-cli >> /dev/null;
-if [ $? -eq 1 ]
-  then
-    echo "sms-notify-cli utility not detected. Preparing to install.";
-    yarn global add @jpfulton/net-sms-notifier-cli;
-  else
-    echo "Found sms-notify-cli utility. Attempting update.";
-    yarn global upgrade @jpfulton/net-sms-notifier-cli@latest;
-fi
+setup-sms-notifier () {
+  # Install or upgrade sms-notify-cli utility
+  which sms-notify-cli >> /dev/null;
+  if [ $? -eq 1 ]
+    then
+      echo "sms-notify-cli utility not detected. Preparing to install.";
+      yarn global add @jpfulton/net-sms-notifier-cli;
+    else
+      echo "Found sms-notify-cli utility. Attempting update.";
+      yarn global upgrade @jpfulton/net-sms-notifier-cli@latest;
+  fi
 
-# Initialize sms-notify-cli configuration
-NOTIFIER_CONFIG="/etc/sms-notifier/notifier.json";
-if [ -f $NOTIFIER_CONFIG ]
-  then
-    echo "Found notifier configuration. Validating with current version.";
+  # Initialize sms-notify-cli configuration
+  local NOTIFIER_CONFIG="/etc/sms-notifier/notifier.json";
+  if [ -f $NOTIFIER_CONFIG ]
+    then
+      echo "Found notifier configuration. Validating with current version.";
 
-    sms-notify-cli validate;
-    if [ $? -eq 0 ]
-      then
-        echo "Configuration file validation passes on current version.";
-      else
-        echo "Invalid configuration file. Manually correct.";
-    fi
-  else
-    echo "No notifier configuration found. Initializing...";
-    echo "Manual configuration to the ${NOTIFIER_CONFIG} file will be required.";
-    sms-notify-cli init;
-fi
+      sms-notify-cli validate;
+      if [ $? -eq 0 ]
+        then
+          echo "Configuration file validation passes on current version.";
+        else
+          echo "Invalid configuration file. Manually correct.";
+      fi
+    else
+      echo "No notifier configuration found. Initializing...";
+      echo "Manual configuration to the ${NOTIFIER_CONFIG} file will be required.";
+      sms-notify-cli init;
+  fi
+}
 
-# Set up eviction query and shutdown script
-EVICTION_QUERY_CRON_SNIPPET_FILE="preempt-query";
-EVICTION_QUERY_SCRIPT="query-for-preempt-event.sh";
-echo "Setting up eviction query script...";
+setup-eviction-shutdown-system () {
+  # Set up eviction query and shutdown script
+  local EVICTION_QUERY_CRON_SNIPPET_FILE="preempt-query";
+  local EVICTION_QUERY_SCRIPT="query-for-preempt-event.sh";
+  echo "Setting up eviction query script...";
 
-wget ${BASE_REPO_URL}/usr/local/sbin/${EVICTION_QUERY_SCRIPT};
-chmod ug+x ./${EVICTION_QUERY_SCRIPT}
-mv ./${EVICTION_QUERY_SCRIPT} /usr/local/sbin/
+  wget ${BASE_REPO_URL}/usr/local/sbin/${EVICTION_QUERY_SCRIPT};
+  chmod ug+x ./${EVICTION_QUERY_SCRIPT}
+  mv ./${EVICTION_QUERY_SCRIPT} /usr/local/sbin/
 
-wget ${BASE_REPO_URL}/etc/cron.d/${EVICTION_QUERY_CRON_SNIPPET_FILE};
-mv ./${EVICTION_QUERY_CRON_SNIPPET_FILE} /etc/cron.d/
+  wget ${BASE_REPO_URL}/etc/cron.d/${EVICTION_QUERY_CRON_SNIPPET_FILE};
+  mv ./${EVICTION_QUERY_CRON_SNIPPET_FILE} /etc/cron.d/
 
-echo "---";
-echo;
+  echo "---";
+  echo;
+}
 
-# Install nmap as needed
-which nmap >> /dev/null;
-if [ $? -eq 1 ]
-  then
-    echo "nmap not detected. Preparing to install.";
-    apt install -y nmap;
-fi
+setup-nmap () {
+  # Install nmap as needed
+  which nmap >> /dev/null;
+  if [ $? -eq 1 ]
+    then
+      echo "nmap not detected. Preparing to install.";
+      apt install -y nmap;
+  fi
+}
 
-# Set up OpenVPN scripts if OpenVPN is installed
-OPENVPN_DIR="/etc/openvpn/";
-BASE_CLIENT_CONFIG="base-client-config.ovpn";
-CLIENT_CONFIG_SCRIPT="create-client-openvpn-config.sh";
-OPENVPN_SCRIPTS_DIR="/etc/openvpn/scripts/";
-CONNECT_SCRIPT="on-connect.sh";
-DISCONNECT_SCRIPT="on-disconnect.sh";
-VERIFY_SCRIPT="on-tls-verify.sh";
-if [ -d $OPENVPN_DIR ]
-  then
-    echo "OpenVPN configuration folder exists.";
+setup-openvpn-support-scripts () {
+  # Set up OpenVPN scripts if OpenVPN is installed
+  local OPENVPN_DIR="/etc/openvpn/";
+  local BASE_CLIENT_CONFIG="base-client-config.ovpn";
+  local CLIENT_CONFIG_SCRIPT="create-client-openvpn-config.sh";
+  local OPENVPN_SCRIPTS_DIR="/etc/openvpn/scripts/";
+  local CONNECT_SCRIPT="on-connect.sh";
+  local DISCONNECT_SCRIPT="on-disconnect.sh";
+  local VERIFY_SCRIPT="on-tls-verify.sh";
+  if [ -d $OPENVPN_DIR ]
+    then
+      echo "OpenVPN configuration folder exists.";
 
-    echo "Installing OpenVPN client template...";
-    wget ${BASE_REPO_URL}${OPENVPN_DIR}${BASE_CLIENT_CONFIG};
-    mv ./${BASE_CLIENT_CONFIG} ${OPENVPN_DIR}${BASE_CLIENT_CONFIG};
+      setup-nmap;
 
-    echo "Installing OpenVPN client config generation script...";
-    wget ${BASE_REPO_URL}${HOME_DIR}${CLIENT_CONFIG_SCRIPT};
-    chmod a+x ${CLIENT_CONFIG_SCRIPT};
+      echo "Installing OpenVPN client template...";
+      wget ${BASE_REPO_URL}${OPENVPN_DIR}${BASE_CLIENT_CONFIG};
+      mv ./${BASE_CLIENT_CONFIG} ${OPENVPN_DIR}${BASE_CLIENT_CONFIG};
 
-    echo "Installing OpenVPN scripts...";
-    if [ ! -d $OPENVPN_SCRIPTS_DIR ]
-      then
-        echo "Creating scripts directory.";
-        mkdir $OPENVPN_SCRIPTS_DIR;
-    fi
+      echo "Installing OpenVPN client config generation script...";
+      wget ${BASE_REPO_URL}${HOME_DIR}${CLIENT_CONFIG_SCRIPT};
+      chmod a+x ${CLIENT_CONFIG_SCRIPT};
 
-    wget ${BASE_REPO_URL}${OPENVPN_SCRIPTS_DIR}${CONNECT_SCRIPT};
-    chmod a+x ./${CONNECT_SCRIPT};
-    mv ./${CONNECT_SCRIPT} ${OPENVPN_SCRIPTS_DIR}${CONNECT_SCRIPT};
+      echo "Installing OpenVPN scripts...";
+      if [ ! -d $OPENVPN_SCRIPTS_DIR ]
+        then
+          echo "Creating scripts directory.";
+          mkdir $OPENVPN_SCRIPTS_DIR;
+      fi
 
-    wget ${BASE_REPO_URL}${OPENVPN_SCRIPTS_DIR}${DISCONNECT_SCRIPT};
-    chmod a+x ./${DISCONNECT_SCRIPT};
-    mv ./${DISCONNECT_SCRIPT} ${OPENVPN_SCRIPTS_DIR}${DISCONNECT_SCRIPT};
+      wget ${BASE_REPO_URL}${OPENVPN_SCRIPTS_DIR}${CONNECT_SCRIPT};
+      chmod a+x ./${CONNECT_SCRIPT};
+      mv ./${CONNECT_SCRIPT} ${OPENVPN_SCRIPTS_DIR}${CONNECT_SCRIPT};
 
-    wget ${BASE_REPO_URL}${OPENVPN_SCRIPTS_DIR}${VERIFY_SCRIPT};
-    chmod a+x ./${VERIFY_SCRIPT};
-    mv ./${VERIFY_SCRIPT} ${OPENVPN_SCRIPTS_DIR}${VERIFY_SCRIPT};
-fi
+      wget ${BASE_REPO_URL}${OPENVPN_SCRIPTS_DIR}${DISCONNECT_SCRIPT};
+      chmod a+x ./${DISCONNECT_SCRIPT};
+      mv ./${DISCONNECT_SCRIPT} ${OPENVPN_SCRIPTS_DIR}${DISCONNECT_SCRIPT};
+
+      wget ${BASE_REPO_URL}${OPENVPN_SCRIPTS_DIR}${VERIFY_SCRIPT};
+      chmod a+x ./${VERIFY_SCRIPT};
+      mv ./${VERIFY_SCRIPT} ${OPENVPN_SCRIPTS_DIR}${VERIFY_SCRIPT};
+  fi
+}
+
+main () {
+  upgrade-base-packages;
+  setup-firewall-defaults-and-ssh;
+  setup-motd;
+  setup-nodejs;
+  setup-yarn;
+  setup-sms-notifier;
+  setup-eviction-shutdown-system;
+  setup-openvpn-support-scripts;
+}
+
+main;
